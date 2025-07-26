458d9e23e8b899007e6ee2dc2b7b6ee2cf26d899
vicalcaz <47284013+vicalcaz@users.noreply.github.com>
2025-07-26 16:39:01 -0300

    cambios mail

--- api/sendMail.mjs
+++ api/sendMail.mjs
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.redacero.com.ar',
     port: 587, // Cambia el puerto si tu proveedor indica otro (ej: 465 para SSL)
-    secure: false, // true si usas SSL (puerto 465)
+    secure: true, // true si usas SSL (puerto 465)
     auth: {
       user: process.env.SMTP_USER || 'encuentro2025@redacero.com.ar',
       pass: process.env.SMTP_PASS || 'Encuentro2025!'
